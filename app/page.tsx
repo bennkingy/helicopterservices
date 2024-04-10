144a77a79781d9d722730fb049805f871fb27bed
Benn Dev <bennkingy@gmail.com>
2024-04-11 00:27:05 +0100

    Added shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent } from "@/components/ui/card";
 import { helicopter } from "./lib/interafce";
 import { client } from "./lib/sanity";
 
@@ -23,11 +24,12 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-5xl font-bold">Helicopter Services</h1>
-        {data.map((post: helicopter, i) => (
-          <div key={i}>
+        {data.map((post: helicopter, idx) => (
+               <Card key={idx}> <CardContent className="mt-5">
             <h2>Model: {post.model}</h2>
-            <p>Capacity: {post.capacity}</p>
-          </div>
+            <p>Capacity: {post.capacity}</p>          </CardContent>
+
+            </Card>
         ))}
     </main>
   );
