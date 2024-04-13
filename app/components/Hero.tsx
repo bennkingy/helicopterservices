dff55626921fb3ed68015d08706b2ad8a9ee5b4a
Benn Dev <bennkingy@gmail.com>
2024-04-13 15:20:01 +0100

    hero update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero = ({ height = 'h-[70vh]', cta, cta2, title, subtitle }: props) => {
       <div className={`backdrop-brightness-50 ${height}`}>
         <div className='h-full grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12'>
           <div className='mr-auto place-self-center lg:col-span-6 mt-[-96px]'>
-            <h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white text-white'>
+            <h1 className='max-w-2xl mb-4 text-4xl tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white text-white'>
               {title}
             </h1>
             <p className='max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-white'>
@@ -46,22 +46,23 @@ const Hero = ({ height = 'h-[70vh]', cta, cta2, title, subtitle }: props) => {
             )}
           </div>
         </div>
-        <div className="absolute bottom-0 left-0 w-full h-24 bg-brand-dark-blue bg-opacity-50 dark:bg-black dark:bg-opacity-50 flex items-center justify-center text-white text-center">
-          <p className="mr-10">A leading UK CAA and EASA approved Training Organisation</p>
-          <Image
-            src="/images/easa.svg"
-            alt="Helicopter Services"
-            width={100}
-            height={34}
-            className='min-w-[100px]'
-          />
-          <Image
-            src="/images/caa.svg"
-            alt="Helicopter Services"
-            width={50}
-            height={34}
-            className="ml-5"
-          />
+        <div className="absolute bottom-0 left-0 w-full min-h-24 py-5 bg-brand-dark-blue bg-opacity-50 dark:bg-black dark:bg-opacity-50 flex flex-col sm:flex-row items-center justify-center text-white text-center">
+          <p className="mb-4 sm:mb-0 sm:mr-10">A leading UK CAA and EASA approved Training Organisation</p>
+          <div className="flex justify-center space-x-5">
+            <Image
+              src="/images/easa.svg"
+              alt="Helicopter Services"
+              width={100}
+              height={34}
+              className='min-w-[100px]'
+            />
+            <Image
+              src="/images/caa.svg"
+              alt="Helicopter Services"
+              width={50}
+              height={34}
+            />
+          </div>
         </div>
       </div>
     </section>
