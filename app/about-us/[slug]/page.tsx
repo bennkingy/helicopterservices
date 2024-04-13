e61066e4ec731844d3e7575859e1b22f61ef9815
Benn Dev <bennkingy@gmail.com>
2024-04-13 13:12:37 +0100

    carousel dots added

---
+++ app/about-us/[slug]/page.tsx
@@ -0,0 +1,61 @@
+import { training } from "@/app/lib/interafce";
+import { client, urlFor } from "@/app/lib/sanity";
+import { PortableText } from "@portabletext/react";
+import type { Metadata } from 'next';
+import Image from "next/image";
+
+export const revalidate = 30; // revalidate at most 30 seconds
+
+async function getData(slug: string) {
+  const query = `
+    *[_type == "about" && slug.current == '${slug}'] {
+        "currentSlug": slug.current,
+          title,
+          seoTitle,
+          seoDescription,
+          body,
+          mainImage,
+      }[0]`;
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+  const data: training = await getData(params.slug.toLowerCase());
+
+  return {
+    title: data?.seoTitle,
+    description: data?.seoDescription,
+  }
+}
+
+export default async function TrainingPage({ params }: { params: { slug: string } }) {
+  const data: any = await getData(params.slug.toLowerCase());
+
+  return (
+    <main className="max-w-6xl mx-auto px-4">
+      <div className="my-8">
+        <h1>
+          <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">
+            {params.slug}
+          </span>
+          <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+            {data?.title || 'no title'}
+          </span>
+        </h1>
+        {data?.mainImage && <Image
+          src={urlFor(data.mainImage).url()}
+          width={2000}
+          height={800}
+          alt="Title Image"
+          priority
+          className="rounded-lg mt-8 border"
+        />}
+        <div className="mt-8">
+          <PortableText value={data?.body || ''} />
+        </div>
+      </div>
+    </main>
+  );
+}
--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,15 +1,38 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import {
   Carousel as CarouselComponent,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
+  type CarouselApi,
 } from "@/components/ui/carousel";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Carousel = () => {
+  const [api, setApi] = useState<CarouselApi>()
+  const [current, setCurrent] = useState(0)
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+    setCount(api.scrollSnapList().length)
+    setCurrent(api.selectedScrollSnap() + 1)
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1)
+    })
+  }, [api])
+
+  console.log(current)
+
   return (
-    <CarouselComponent>
+    <CarouselComponent setApi={setApi}>
       <CarouselContent className="-ml-2 md:-ml-4">
         <CarouselItem className="sm:basis-1/3 text-center px-20">
           <Image
@@ -55,9 +78,22 @@ const Carousel = () => {
           <p>We offer training from Private Pilots lisence to Commerical, instruments, instructor and examiner ratings.</p>
         </CarouselItem>
       </CarouselContent>
+      <div className="flex center justify-center mt-5">
+        {Array.from(Array(count).keys()).map((i) => (
+          <div key={i}>
+            <Button
+              className={`mx-1 h-1.5 flex-grow rounded-full p-3  ${i === current - 1
+                ? "bg-red-500 hover:bg-green"
+                : "bg-neutral-600/75"
+                }`}
+              onClick={() => api?.scrollTo(i)}
+            />
+          </div>
+        ))}
+      </div>
       <CarouselPrevious className="ml-[50px]" />
       <CarouselNext className="mr-[50px]" />
-    </CarouselComponent>
+    </CarouselComponent >
   )
 }
 
