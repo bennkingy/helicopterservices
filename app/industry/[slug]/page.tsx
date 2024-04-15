0c731fafa8eec823019208d123ea5fc1a64956e0
Benn Dev <bennkingy@gmail.com>
2024-04-16 00:00:58 +0100

    bug to fix

--- app/industry/[slug]/page.tsx
+++ app/industry/[slug]/page.tsx
@@ -2,8 +2,8 @@ import GetinTouchSmall from "@/app/components/GetinTouchSmall";
 import Header from "@/app/components/Header";
 import { training } from "@/lib/interface";
 import { client, urlFor } from "@/lib/sanity";
-import { PortableText } from "@portabletext/react";
 import type { Metadata } from 'next';
+import { PortableText } from "next-sanity";
 import Image from "next/image";
 
 export const revalidate = 30; // revalidate at most 30 seconds
@@ -43,7 +43,7 @@ export default async function TrainingPage({ params }: { params: { slug: string 
       <Header title={data?.title} image={data.mainImage} backgroundPosition={'bg-bottom'} />
       <main className="max-w-6xl mx-auto px-4 grid py-10 grid-cols-1 md:grid-cols-3">
         <div className="pr-0 sm:pr-20 mb-10 sm:mb-0 col-span-2">
-          <div className="animate-in fade-in zoom-in">
+          <div className="animate-in fade-in zoom-in prose prose-a:text-blue-400">
             <PortableText value={data?.body || ''} />
           </div>
         </div>
