9d312641fd7661e6e6f302dc9ecea02610666f13
Benn Dev <bennkingy@gmail.com>
2024-04-21 12:35:39 +0100

    fix

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -13,3 +13,26 @@ const builder = imageUrlBuilder(client);
 export function urlFor(source: any) {
   return builder.image(source);
 }
+
+
+// Function to generate URL and fetch metadata for an image
+// export function urlFor(source: any, includeMetadata = false) {
+//   const url = builder.image(source).url();
+//   if (!includeMetadata) {
+//     return url;
+//   }
+
+//   // Assuming metadata is part of the source object
+//   // This would require your queries fetching 'source' to include metadata fields
+//   const metadata = {
+//     dimensions: source.metadata.dimensions,
+//     lqip: source.metadata.lqip,
+//   };
+
+//   console.log(metadata)
+  
+//   return {
+//     url,
+//     metadata,
+//   };
+// // }
