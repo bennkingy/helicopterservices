837d6db52f4d306a53bc71de53818cd0bfab9e24
Benn Dev <bennkingy@gmail.com>
2024-04-28 23:15:58 +0100

    clean up

--- app/components/Enquire.tsx
+++ app/components/Enquire.tsx
@@ -8,9 +8,9 @@ interface props {
 const Enquire = ({ className, textStyle }: props) => {
   return (
     <>
-      <div className={`flex items-center ${className}`}>
-        <Button className='uppercase bg-brand-light-blue text-white min-w-[150px]'>Enquire Today</Button>
-        <div className={`ml-4 font-openSans ${textStyle}`}>
+      <div className={`flex sm:items-center flex-col sm:flex-row ${className}`}>
+        <Button className='uppercase bg-brand-light-blue text-white w-[150px]'>Enquire Today</Button>
+        <div className={`mt-5 sm:mt-0 sm:ml-4 font-openSans ${textStyle}`}>
           <p className='-mb-1'>Call General enquiries:</p>
           <a href="tel:+441494513166" className='text-2xl font-bold mt-0'>+44 1494 513 166</a>
         </div>
