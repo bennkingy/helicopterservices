7b4aea463427b3ee59e2d9478388a0cb57cc6143
Benn Dev <bennkingy@gmail.com>
2024-04-29 16:54:28 +0100

    updates

--- app/flights/[slug]/page.tsx
+++ app/flights/[slug]/page.tsx
@@ -1,12 +1,9 @@
-import ContactCta from "@/app/components/ContactCta";
-import GetinTouchSmall from "@/app/components/GetinTouchSmall";
-import Header from "@/app/components/Header";
-import Map from '@/app/components/Map';
+import Template from "@/app/components/Template";
 import TransferTimesTable from "@/app/components/TransferTimesTable";
 import { training } from "@/lib/interface";
 import { client } from "@/lib/sanity";
-import { PortableText } from "@portabletext/react";
 import type { Metadata } from 'next';
+
 export const revalidate = 30; // revalidate at most 30 seconds
 
 async function getData(slug: string) {
@@ -39,25 +36,13 @@ export default async function FlightsgPage({ params }: { params: { slug: string 
 
   return (
     <>
-      <Header title={data?.title} image={data.mainImage} />
-      <main className="container mx-auto px-4 grid py-16 grid-cols-1 md:grid-cols-3 overflow-x-hidden">
-        <div className="pr-0 sm:pr-20 mb-10 sm:mb-0 col-span-2">
-          <div className="prose prose-a:text-brand-light-blue font-openSans prose-h2:font-workSans prose-h2:text-4xl prose-strong:font-bold marker:text-brand-light-blue max-w-full">
-            <PortableText value={data?.body || ''} />
+      <Template data={data}>
+        {showTransferTimesTable && (
+          <div className="mt-10">
+            <TransferTimesTable />
           </div>
-          {showTransferTimesTable && (
-            <div className="mt-10">
-              {/* <h2 className="text-2xl font-bold mb-5">Transfer Times</h2> */}
-              <TransferTimesTable />
-            </div>
-          )}
-        </div>
-        <div>
-          <ContactCta />
-          <GetinTouchSmall className='mt-3' />
-          <Map className='h-[400px] mt-3' />
-        </div>
-      </main >
+        )}
+      </Template>
     </>
   );
 }
