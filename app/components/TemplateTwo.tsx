6cf113288255dbf18bc25309308f1a1bd2061b94
Benn Dev <bennkingy@gmail.com>
2024-05-03 23:34:48 +0100

    fleet page looking sick!

--- app/components/TemplateTwo.tsx
+++ app/components/TemplateTwo.tsx
@@ -9,7 +9,6 @@ import { FleetItem } from '../fleet/[slug]/page';
 import Breadcrumbs from "./Breadcrumbs";
 import Gallery from './Gallery';
 import TextLink from './TextLink';
-import ThreeDimensionVideo from './ThreeDimensionVideo';
 
 type props = {
   data: FleetItem;
@@ -201,7 +200,29 @@ const TemplateTwo = ({ data, children }: props) => {
               alt="hero image example"
             />
           </div>
-          <ThreeDimensionVideo openModal={isModalOpen} onClose={() => setIsModalOpen(!isModalOpen)} />
+          <div style={{ width: '100%', clipPath: 'polygon(0 0,calc(100% - 20px) 0, 100% 20px,100% 100%, 0 100%)' }}>
+            <Gallery className='sm:mb-10' galleryType="video">
+              <a
+                data-lg-size="1000-700"
+                data-pinterest-text="Pin it2"
+                data-tweet-text="lightGallery slide  2"
+                className="gallery__item"
+                data-src="https://www.youtube.com/embed/JOpWTNEAsX8?si=EbI2oaTH5s4boI_Y"
+                data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@therawhunter' >Massimiliano Morosinotto </a></h4><p> Location - <a href='https://unsplash.com/s/photos/tre-cime-di-lavaredo%2C-italia'>Tre Cime di Lavaredo, Italia</a>This is the Way</p>"
+              >
+                <div style={{ position: 'relative', width: '100%', paddingBottom: '100%' }}>
+                  <Image
+                    priority
+                    src={'/images/cockpit2.png'}
+                    layout='fill'
+                    objectFit='cover'
+                    alt="hero image example"
+                    className="cursor-pointer"
+                  />
+                </div>
+              </a>
+            </Gallery>
+          </div>
           <div className='bg-brand-medium-blue px-5 py-4 border-b-4 border-brand-light-blue'>
             <p className='text-white font-bold font-openSans'>This Helicopter is based at:</p>
             <p className='text-white font-openSans font-thin'>White Waltham Airfield, Maidenhead, Berkshire, SL6 3N</p>
