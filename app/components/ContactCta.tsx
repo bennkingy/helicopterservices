a3e83c056a44696596e526a84ce501f0d83cde77
Benn Dev <bennkingy@gmail.com>
2024-05-07 23:25:26 +0100

    props

--- app/components/ContactCta.tsx
+++ app/components/ContactCta.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 
-const ContactCta = ({ className }) => {
+type props = {
+	className?: string;
+};
+
+const ContactCta = ({ className }: props) => {
 	return (
 		<div className={`text-white bg-brand-medium-blue h-fit ${className}`}>
 			<div className="bg-brand-medium-blue py-5 text-white p-10 relative">
