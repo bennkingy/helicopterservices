55380c86941cb66a48cedb0beb8f311fa8621635
Benn Dev <bennkingy@gmail.com>
2024-05-07 23:13:37 +0100

    fix

--- app/components/ComparisonTable.tsx
+++ app/components/ComparisonTable.tsx
@@ -319,7 +319,7 @@ export function ComparisonTable() {
 			/>
 			<Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
 				<div className="w-full">
-					<div className="fixed bg-white w-full z-[2] shadow-md	 sm:shadow-none">
+					<div className="fixed bg-white w-full z-[2] shadow-md	sm:shadow-none">
 						<div className="border-b-4 border-brand-light-blue relative bg-white">
 							<h3 className="font-workSans text-xl sm:text-2xl text-brand-dark-blue p-4 font-extrabold mt-0">
 								Compare fleet helicopters
