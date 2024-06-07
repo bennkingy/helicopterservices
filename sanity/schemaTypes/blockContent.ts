6503a9210159415bcdf420d805adb4a127172c2c
Benn Dev <bennkingy@gmail.com>
2024-06-07 22:46:27 +0100

    url fix

--- sanity/schemaTypes/blockContent.ts
+++ sanity/schemaTypes/blockContent.ts
@@ -50,6 +50,11 @@ export default defineType({
 								title: "URL",
 								name: "href",
 								type: "url",
+								validation: (Rule) =>
+									Rule.uri({
+										allowRelative: true,
+										scheme: ["http", "https", "mailto", "tel"],
+									}),
 							},
 						],
 					},
