c8851402e9266b4e86c54707aca427759cbbdb5f
Benn Dev <bennkingy@gmail.com>
2024-06-21 23:00:52 +0100

    test

--- app/components/MobileMenu.tsx
+++ app/components/MobileMenu.tsx
@@ -113,6 +113,8 @@ const MobileMenu = ({ onMobileOpen }: { onMobileOpen: () => void }) => {
 		setActiveMenu("main");
 	};
 
+	console.log("activeMenu", activeMenu);
+
 	// useEffect(() => {
 	// 	const handleResize = () => {
 	// 		if (window.innerWidth > 767 && menuOpen) {
