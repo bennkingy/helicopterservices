d0f1de4fc2f26fd1e974e0a4134070eeda1c7ebc
Benn Dev <bennkingy@gmail.com>
2024-06-30 21:25:50 +0100

    fix

--- app/components/Template.tsx
+++ app/components/Template.tsx
@@ -121,7 +121,7 @@ const Template = ({
 					{ "md:grid-cols-8": sidebar, "md:grid-cols-1": !sidebar },
 				)}
 			>
-				<div className="pr-0 md:pr-20 mb-16 md:mb-0 col-span-8 lg:col-span-5">
+				<div className="pr-0 md:pr-20 mb-16 md:mb-0 col-span-8 md:col-span-5">
 					<div className="prose prose-a:text-brand-orange prose-a:transition-colors hover:prose-a:text-brand-dark-blue prose-a:no-underline font-openSans prose-h2:font-workSans prose-h2:text-3xl sm:prose-h2:text-4xl prose-strong:font-bold marker:text-brand-light-blue max-w-full text-brand-dark-grey">
 						<PortableText value={data?.body || ""} components={components} />
 					</div>
@@ -183,7 +183,7 @@ const Template = ({
 					</>
 				)} */}
 				{sidebar && (
-					<div className="col-span-3 lg:col-span-3">
+					<div className="col-span-3">
 						<ContactCta className="hidden md:block" pilot={data?.pilot} />
 						<GetinTouchSmall className="mt-20 md:mt-8 hidden md:block" />
 						<GMapHeader className="" />
