ee7d67732171e7047e917018ba75eb2e9a7db30f
Benn Dev <bennkingy@gmail.com>
2024-07-21 18:27:43 +0100

    updated arrow colour

--- app/components/ScrollTop.tsx
+++ app/components/ScrollTop.tsx
@@ -6,7 +6,7 @@ import ScrollToTop from "react-scroll-up";
 const ScrollTop = () => {
 	return (
 		<ScrollToTop showUnder={160} duration={1000}>
-			<Icons.moveUp className="h-8 w-8 shadow-2xl text-brand-light-blue z-[100]" />
+			<Icons.moveUp className="h-8 w-8 shadow-2xl text-brand-orange z-[100]" />
 		</ScrollToTop>
 	);
 };
