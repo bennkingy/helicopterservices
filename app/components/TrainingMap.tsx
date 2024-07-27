576d822b3d5b77160a5ad36590f43a3f0f9c4ea2
Benn Dev <bennkingy@gmail.com>
2024-07-27 17:25:24 +0100

    training map

--- app/components/TrainingMap.tsx
+++ app/components/TrainingMap.tsx
@@ -1,6 +1,6 @@
 const TrainingMap = () => {
 	return (
-		<div className="sm:px-20 mb-10 sm:mb-16 max-w-[1000px] mx-auto">
+		<div className="sm:px-20 mb-10 sm:mb-16 max-w-[960px] mx-auto">
 			<table className="w-full border-separate border-spacing-0 table-fixed">
 				<tbody>
 					<tr className="row2">
@@ -23,20 +23,18 @@ const TrainingMap = () => {
 						<td className="p-2 sm:p-3" />
 						<td className="p-2 sm:p-3" />
 						<td className="p-2 sm:p-3" />
-						<td colSpan={2} className="p-2 sm:p-3">
+						<td colSpan={1} className="p-2 sm:p-3">
 							<div className="shadow-sm sm:shadow-lg cursor-pointer">
 								<div
-									className="bg-white flex items-center justify-center"
+									className="aspect-square bg-white flex items-center justify-center"
 									style={{
 										clipPath:
 											"polygon(0 0,calc(100% - 20px) 0,100% 20px,100% 100%,0 100%)",
 										maxHeight: "136px",
 									}}
 								>
-									<div className="font-workSans font-bold text-[10px] sm:text-base text-center py-5 sm:py-10 max-h-[162px]">
-										Advanced flying
-										<br />
-										programme
+									<div className="font-workSans font-bold text-[10px] sm:text-base py-5 sm:py-10">
+										&nbsp;&nbsp;&nbsp;AFP&nbsp;&nbsp;&nbsp;
 									</div>
 								</div>
 							</div>
@@ -61,7 +59,6 @@ const TrainingMap = () => {
 								</div>
 							</div>
 						</td>
-						<td />
 						<td />
 						<td />
 					</tr>
@@ -109,7 +106,6 @@ const TrainingMap = () => {
 						</td>
 						<td className="p-2 sm:p-3" />
 						<td className="p-2 sm:p-3" />
-						<td className="p-2 sm:p-3" />
 					</tr>
 					<tr className="row4" style={{ backgroundColor: "#E3E9F1" }}>
 						<td className="p-2 sm:p-3" />
@@ -146,17 +142,17 @@ const TrainingMap = () => {
 								</div>
 							</div>
 						</td>
-						<td colSpan={2} className="p-2 sm:p-3">
+						<td colSpan={1} className="p-2 sm:p-3">
 							<div className="shadow-sm sm:shadow-lg cursor-pointer">
 								<div
-									className="bg-white flex items-center justify-center"
+									className="aspect-square bg-white flex items-center justify-center"
 									style={{
 										clipPath:
 											"polygon(0 0,calc(100% - 20px) 0,100% 20px,100% 100%,0 100%)",
 										maxHeight: "136px",
 									}}
 								>
-									<div className="font-workSans font-bold text-[10px] sm:text-base text-center py-5 sm:py-10 max-h-[162px]">
+									<div className="font-workSans font-bold text-[10px] sm:text-base">
 										Simulator
 									</div>
 								</div>
@@ -183,17 +179,17 @@ const TrainingMap = () => {
 								</div>
 							</div>
 						</td>
-						<td colSpan={2} className="p-2 sm:p-3">
+						<td colSpan={1} className="p-2 sm:p-3">
 							<div className="shadow-sm sm:shadow-lg cursor-pointer">
 								<div
-									className="bg-white flex items-center justify-center"
+									className="aspect-square bg-white flex items-center justify-center"
 									style={{
 										clipPath:
 											"polygon(0 0,calc(100% - 20px) 0,100% 20px,100% 100%,0 100%)",
 										maxHeight: "136px",
 									}}
 								>
-									<div className="font-workSans font-bold text-[10px] sm:text-base text-center py-5 sm:py-10 max-h-[162px]">
+									<div className="font-workSans font-bold text-[10px] sm:text-base">
 										PBN
 									</div>
 								</div>
@@ -253,7 +249,6 @@ const TrainingMap = () => {
 							</div>
 						</td>
 						<td className="p-2 sm:p-3" />
-						<td className="p-2 sm:p-3" />
 					</tr>
 					<tr className="row7" style={{ backgroundColor: "#E3E9F1" }}>
 						<td className="p-2 sm:p-3">
@@ -307,7 +302,6 @@ const TrainingMap = () => {
 								</div>
 							</div>
 						</td>
-						<td className="p-2 sm:p-3" />
 						<td className="p-2 sm:p-3" />
 					</tr>
 				</tbody>
