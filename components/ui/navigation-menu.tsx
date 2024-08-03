e5b43b524de2d2e5b7e39101786b4cb4a90d0ee9
Benn Dev <bennkingy@gmail.com>
2024-08-03 12:20:15 +0100

    removed focus

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -40,7 +40,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-	"group inline-flex h-[96px] w-max items-center justify-center bg-background px-4 py-2 text-md transition-colors hover:bg-accent hover:text-accent-foreground  focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+	"group inline-flex h-[96px] w-max items-center justify-center bg-background px-4 py-2 text-md transition-colors hover:bg-accent hover:text-accent-foreground  focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
 );
 
 const NavigationMenuTrigger = React.forwardRef<
