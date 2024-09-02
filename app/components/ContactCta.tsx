14759eb437039e0530df73dc4eb8d6f56ca94c93
Benn Dev <bennkingy@gmail.com>
2024-09-02 12:10:02 +0100

    fixes

--- app/components/ContactCta.tsx
+++ app/components/ContactCta.tsx
@@ -32,11 +32,15 @@ const ContactCta = ({ className, pilot, service }: props) => {
 			<div className=" text-white relative z-1 overflow-x-hidden pb-3">
 				<p className="font-bold mb-4 pl-5 mt-6 lg:mt-0 pr-3 lg:pr-32 sm:text-lg font-openSans">
 					“Enquire today for a customised{" "}
-					{service !== "Fleet" &&
-					service?.toLowerCase() !== "the hangar" &&
-					service
-						? `${service.toLowerCase()} `
-						: null}
+					{service.toLowerCase() === "ppl(h)"
+						? "private pilot licence  "
+						: service.toLowerCase() === "cpl(h)"
+							? "commerical pilot licence "
+							: service !== "Fleet" &&
+									service?.toLowerCase() !== "the hangar" &&
+									service
+								? `${service.toLowerCase()} `
+								: null}
 					quote tailored to your needs”
 				</p>
 				<p className="pl-5 font-openSans">
